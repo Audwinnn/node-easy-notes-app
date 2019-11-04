6d1879322eb7e0341b6df21504a8932ede79d3db
Audouin Bulanhagui <audouin0033@gmail.com>
2019-11-04 09:19:01 +0800

    Configuring and Connecting to the database

--- node-easy-notes-app/server.js
+++ node-easy-notes-app/server.js
@@ -19,3 +19,19 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
